c82653f4ea4c29c07e2d74347d234a897377d69a
Ruin0x11 <ipickering2@gmail.com>
2021-03-28 18:16:42 -0700

    Play MIDI successfully (Windows, MCI)

--- src/native/windows/mci.rs
+++ src/native/windows/mci.rs
@@ -1,22 +1,30 @@
 use std::ptr::null_mut;
+use anyhow::{anyhow, Result};
 
 use winapi;
-use winapi::shared::ntdef::{LPCSTR, LPCWSTR, LPWSTR};
-use winapi::shared::minwindef::{DWORD, UINT};
-use windows::wide::ToWide;
-use windows::utf;
+use winapi::um::mmsystem::MCIERROR;
+use native::windows::wide::ToWide;
+use native::windows::utf;
 
-pub fn mci_error() -> String {
+pub fn mci_error(err: MCIERROR) -> String {
     unsafe {
-        let mut asd: LPWSTR = null_mut();
-        let mut len = 0;
-        let mut zxc = 0;
-        winapi::um::mmsystem::mciGetErrorStringW(zxc, asd, len);
-        println!("{:?} {}", asd, len);
-        utf::utf16_to_string_n(asd, len as usize)
+        let size = 256;
+        let mut buf = Vec::with_capacity(size);
+        winapi::um::mmsystem::mciGetErrorStringW(err, buf.as_mut_ptr(), size as u32);
+        utf::utf16_to_string(buf.as_ptr())
     }
 }
 
-pub unsafe fn mci_command(command: &str) -> u32 {
-    winapi::um::mmsystem::mciSendStringW(command.to_wide_null().as_ptr(), null_mut(), 0, null_mut())
+pub fn mci_command(command: &str) -> Result<String> {
+    unsafe {
+        let size = 256;
+        let mut buf = Vec::with_capacity(size);
+        let err = winapi::um::mmsystem::mciSendStringW(command.to_wide_null().as_ptr(), buf.as_mut_ptr(), size as u32, null_mut());
+        if err > 0 {
+            Err(anyhow!(mci_error(err)))
+        } else {
+            let s = utf::utf16_to_string(buf.as_ptr());
+            Ok(s)
+        }
+    }
 }
--- src/native/windows/mod.rs
+++ src/native/windows/mod.rs
@@ -1,24 +1,32 @@
 use std::path::Path;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 mod wide;
 mod mci;
 mod utf;
 
 pub fn play_midi<P: AsRef<Path>>(path: P) -> Result<()> {
-    unsafe {
-        let s = format!("open {} alias music", path.as_ref().as_str());
-        mci::mci_command(&s);
-        mci::mci_command("play music");
+    if is_midi_playing() {
+        stop_midi()?;
     }
+
+    mci::mci_command(&format!("open {} alias midi", path.as_ref().to_str().unwrap()))?;
+    mci::mci_command("play midi")?;
 
     Ok(())
 }
 
-pub fn stop_midi() {
-    unsafe {
-        mci::mci_command("stop music");
+pub fn is_midi_playing() -> bool {
+    mci::mci_command("status midi ready").is_ok()
+}
+
+pub fn stop_midi() -> Result<()> {
+    if !is_midi_playing() {
+        return Ok(());
     }
+
+    mci::mci_command("stop midi")?;
+    mci::mci_command("close midi")?;
 
     Ok(())
 }
